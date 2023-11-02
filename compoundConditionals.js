a61faf9cef0b8565b20b47f02123f42e4175fccb
SaulASC23 <139931569+SaulASC23@users.noreply.github.com>
2023-11-02 22:28:39 +0000

    Finished-Version
    Finished Version of the repository.

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -4,18 +4,30 @@
 
 
 // Change these values to test different scenarios
-let mood = "sad";        // Possible values: "happy", "sad", "excited", "bored"
-let timeOfDay = "night"; // Possible values: "morning", "afternoon", "evening", "night"
+let m = "sad";        // Possible values: "happy", "sad", "excited", "bored"
+let t = "night"; // Possible values: "morning", "afternoon", "evening", "night"
 
-let recommendedMovie;
+let rM;
 
 // 1. Implement the recommendation logic using compound conditionals
 // - If the user is "tired" OR it's "night", recommend "Relaxing Ambient Film".
 // - If the user is "excited" AND it's NOT "morning", recommend "High-Octane Thriller".
 // - If the user is "bored" AND it's "afternoon" OR "happy" AND it's "evening", recommend "Engaging Mystery Film".
 // - In all other cases, recommend "Popular Choice Film".
+if (m === "tired" || t === "night"){
+  rM = "Relaxing Ambient Film";
+}
+else if(m === "excited" && t !== "morning"){
+  rM = "High-Octane Thriller";
+}
+else if((m === "bored" && t === "afternoon") || (m === "happy" && t === "evening")) {
+  rM = "Engaging Mystery Film";
+}
+else{
+  rM === "Popular Choice Film";
+}
 
 
 
 // 2. Console log the recommended movie
-
+console.log(rM);
--- conditionals.js
+++ conditionals.js
@@ -3,17 +3,26 @@
 // recommends an ice cream flavor based on the current temperature in Fahrenheit.
 
 // 1. Create a variable named temperature any value between 32 and 68.
-
+let temp = Math.floor(Math.random()*(33))+32;
+console.log
 
 // 2. Create a variable named recommendedFlavor. Do not assign a value.
-
+let recommendedFlavor;
 
 // 3. Implement the recommendation logic
 // - If the temperature is less than 50, recommend "Hot Chocolate Fudge".
 // - If the temperature is between 50 and 68 (inclusive), recommend "Caramel Crunch".
 // - If the temperature is greater than 68, recommend "Lemon Sorbet".
-
+if (temp < 50) {
+  recommendedFlavor = "Hot Chocolate Fudge";
+}
+else if (temp >= 50 && temp <= 68) {
+  recommendedFlavor = "Caramel Crunch";
+}
+else{
+  recommendedFLavor = "Lemon Sorbet";
+}
 
 // 4. Console log the recommended flavor.
-
+console.log(recommendedFlavor);
 
